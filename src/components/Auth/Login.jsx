395c21e3103a5bdb7e97cf2dd6018793f29f4490
adhidhyaRP <153973098+adhidhyaRP@users.noreply.github.com>
2024-11-17 19:45:44 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -24,6 +24,7 @@ const Login = () => {
         .then((response) => {
           if (response.data.status) {
             localStorage.setItem('authorized',true)
+             localStorage.setItem('userId',response.data.userId)
             
             navigate('/');
           }
